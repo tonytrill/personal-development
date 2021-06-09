52c028f7695b608bf61dd1f27905f2b7a9d91525
Tony Silva <tonysilva.ou@gamil.com>
2021-06-09 15:27:52 -0600

    react complete tutorial using state

---
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,26 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import { useState } from "react";
+
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  console.log("ExpenseItem evaulated by React");
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
---
+++ react-complete-guide/src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,24 @@
+import "./ExpensesFilter.css";
+import React from "react";
+
+const ExpensesFilter = (props) => {
+  const dropdownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
+  };
+
+  return (
+    <div className="expenses-filter">
+      <label>Filter By Year</label>
+      <div className="expenses-filter__control">
+        <select onChange={dropdownChangeHandler} defaultValue={props.selected}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
